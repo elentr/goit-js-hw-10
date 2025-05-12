c46e6ea7e34fce02290ece1b203b947c17a49381
elentr <tr.13.elena@gmail.com>
2025-05-12 21:13:30 +0300

    done task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,3 +2,34 @@
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const delay = Number(form.delay.value);
+  const status = form.state.value;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (status === 'fulfilled') {
+        return resolve(delay);
+      } else {
+        return reject(delay);
+      }
+    }, delay);
+  });
+  promise
+    .then(delay => {
+    iziToast.success({
+      message: `✅ Fulfilled promise in ${delay}ms`,
+      position: 'topRight',
+    })
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      })
+    })
+  form.reset();
+})
